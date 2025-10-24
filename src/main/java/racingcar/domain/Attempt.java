b2d87b8d645cf4b7112823897129554a43f0867d
나의찬 <cab07036@naver.com>
2025-10-25 03:19:41 +0900

    refactor(domain): Attempt 클래스 수정
    value 값을 final 변수로 수정하고 불필요한 개행 문자를 삭제하고 주석을 더 명확히 작성하였습니다.

--- src/main/java/racingcar/domain/Attempt.java
+++ src/main/java/racingcar/domain/Attempt.java
@@ -1,10 +1,9 @@
 package racingcar.domain;
 
 public class Attempt {
-    private int value;
+    private final int value;
 
     public Attempt(String value) {
-
         validate(value);
         this.value = Integer.parseInt(value);
     }
@@ -14,12 +13,12 @@ public class Attempt {
     }
 
     private void validate(String value) {
-        // 문자인지 숫자인지 검사
+        // 숫자 아니라면 오류
         if (!value.matches("^\\d+$")) {
             throw new IllegalArgumentException("시도 횟수는 숫자여야 합니다.");
         }
 
-        // 0보다 큰지 검사
+        // 0 이하라면 오류
         int intValue = Integer.parseInt(value);
         if (intValue <= 0) {
             throw new IllegalArgumentException("시도 횟수는 0보다 커야 합니다.");
