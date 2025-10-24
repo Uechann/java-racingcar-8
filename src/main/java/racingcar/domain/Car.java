9aca4582a232bbe2b7c1b94c7df2c1e62fbe06c4
나의찬 <cab07036@naver.com>
2025-10-25 01:25:37 +0900

    feat(domain): Car 클래스 추가 및 속성 및 오퍼레이션 추가
    Car 클래스가 CarName을 파라미터로 생성되었을 때 CarName이 올바르게 속성으로 들어가도록 구현하였습니다.
    
    그리고 getName() 을 통해 CarName을 반환하도록 구현하였습니다.

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+public class Car {
+    private CarName name;
+
+    public Car(CarName name) {
+        this.name = name;
+    }
+
+    public CarName getName() {
+        return name;
+    }
+}
