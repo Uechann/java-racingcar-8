493d8331ded8822cde0175139cd485491b8dcff4
나의찬 <cab07036@naver.com>
2025-10-25 02:24:48 +0900

    test(domain): 자동차 팩토리 클래스 테스트 코드 작성
    CarFactory에서 파싱된 문자열을 매개변수로 하여 createCars 메서드를 호출했을 때 같은 사이즈가 존재하는지, 이름이 같은지 테스트하는 테스트 코드를 작성하였습니다.
    
    또한, 동일한 이름이 전달되었을 때 IllegalArgumentException이 throw가 되는지 test 코드를 작성하였습니다.
    
    또한, 추후에 확장을 고려하여 CarFactory에서 자동차의 이름으로 검색하여 객체를 받아오는지 테스트 하는 코드를 작성하였습니다.

---
+++ src/test/java/racingcar/domain/CarFactoryTest.java
@@ -0,0 +1,54 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CarFactoryTest {
+
+    CarFactory carFactory = new CarFactory();
+
+    @Test
+    void 자동차_객체들_생성_테스트() {
+        // Given
+        String[] input = {"pobi", "woni", "jun"};
+
+        // When
+        Cars cars = carFactory.createCars(input);
+
+        // Then
+        // 각 자동차 객체들이 정상적으로 생성되었는지 확인
+        assertThat(cars.getCars()).hasSize(3);
+        assertThat(cars.getCars().get(0).getName().value()).isEqualTo("pobi");
+        assertThat(cars.getCars().get(1).getName().value()).isEqualTo("woni");
+        assertThat(cars.getCars().get(2).getName().value()).isEqualTo("jun");
+    }
+
+    @Test
+    void 자동차_이름_중복_테스트() {
+        // Given
+        String[] input = {"pobi", "woni", "pobi"};
+
+        // When
+        Cars cars = carFactory.createCars(input);
+
+        // Then
+        assertThatThrownBy(() -> carFactory.createCars(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 자동차_이름으로_검색_테스트() {
+        // Given
+        String[] input = {"pobi", "woni", "jun"};
+        Cars cars = carFactory.createCars(input);
+
+        // When
+        Car car = cars.findByName("woni");
+
+        // Then
+        assertThat(car).isNotNull();
+        assertThat(car.getName().value()).isEqualTo("woni");
+    }
+}
