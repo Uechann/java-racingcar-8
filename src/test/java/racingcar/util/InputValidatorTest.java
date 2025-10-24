68f3e258c6653ad11a762874b060a50285b80e3f
나의찬 <cab07036@naver.com>
2025-10-25 00:58:38 +0900

    test(util): 입력값 검증 테스트 코드 작성 (RED)
    먼저 입력에 대한 테스트 코드를 작성하려고 합니다.
    올바른 입력 형식에 대해선 true를 반환하고
    
    올바르지 않은 테스트의 경우에는 IllegalArgumentException을 throw하도록 작성하였습니다.

---
+++ src/test/java/racingcar/util/InputValidatorTest.java
@@ -0,0 +1,28 @@
+package racingcar.util;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class InputValidatorTest {
+
+    @Test
+    void 입력형식_검증_테스트() {
+        String input = "pobi,woni,jun";
+        boolean isValid = InputValidator.validateInputFormat(input);
+
+        assertThat(isValid).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "   ", "pobi;woni;jun", "pobi woni jun", "pobi,,jun", "pobi,woni,", ",pobi,woni"})
+    void 입력형식_검증_실패_테스트() {
+        String input = "pobi woni jun"; // 잘못된 형식
+
+        assertThatThrownBy(() -> InputValidator.validateInputFormat(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
