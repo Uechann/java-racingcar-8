3d159cd3840b32e2d605bdaa3939626384f7c338
나의찬 <cab07036@naver.com>
2025-10-26 01:25:23 +0900

    refactor(view): RacingGame 시도 횟수별 출력 연결
    RacingGame 실행 중에 각 시도 횟수마다 자동차들의 위치를 출력하는 함수를 추가하여 바로바로 각 반복문 마다 출력이 되도록 구현하였습니다.

--- src/main/java/racingcar/domain/service/RacingGame.java
+++ src/main/java/racingcar/domain/service/RacingGame.java
@@ -4,20 +4,25 @@ import racingcar.domain.Attempt;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
 import racingcar.util.RandomNumberGenerator;
+import racingcar.view.OutputView;
 
 public class RacingGame {
 
+    private final OutputView outputView = new OutputView();
     private static final int MOVEMENT_CRITERIA = 4;
 
     // 게임 실행
     public void run(Cars cars, Attempt attempt) {
         for (int i = 0; i < attempt.value(); i++) {
             cars.getCars().forEach(this::attemptOneTry);
+
+            // 각 시도마다 자동차 위치 출력
+            outputView.printCarsPosition(cars);
         }
     }
 
     private void attemptOneTry(Car car) {
-        // 랜점 숫자 생성 후 판단
+        // 랜덤 숫자 생성 후 판단
         int num = RandomNumberGenerator.generate();
         judgeMovement(car, num);
     }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,21 +4,22 @@ import racingcar.domain.Cars;
 
 public class OutputView {
 
-    public static final String RESULT_MESSAGE = "\n실행 결과";
     static StringBuilder sb = new StringBuilder();
 
+    // 자동차들의 현재 위치를 출력하는 메서드
     public void printCarsPosition(Cars cars) {
-        sb.append(RESULT_MESSAGE).append("\n");
+
+        sb.append("\n").append("실행 결과").append("\n");
 
         cars.getCars().forEach(car -> {
             sb.append(car.getName().value()).append(" : ");
 
+            // 현재 위치만큼 '-' 출력
             int position = car.getPosition();
             sb.append("-".repeat(position));
 
             sb.append("\n");
         });
-
         System.out.print(sb);
     }
 }
