f79d7ac302234755b0b0f519c35298c64bfa1c8d
나의찬 <cab07036@naver.com>
2025-10-26 00:28:47 +0900

    test(domain): RacingGame 자동차 움직임 테스트 코드 작성
    레이싱 게임이 진행된 후 각 시도횟수마다 생성된 랜덤 숫자 별로 움식임은 판단하고 자동차가 올바르게 위치가 변경이 되었는지 확인하는 테스트 코드를 작성하였습니다.

---
+++ src/test/java/racingcar/domain/service/RacingGameTest.java
@@ -0,0 +1,33 @@
+package racingcar.domain.service;
+
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.domain.CarName;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingGameTest {
+
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+    private final RacingGame racingGame = new RacingGame();
+
+    @Test
+    void 랜덤_숫자_생성으로_자동차_움직임_테스트() {
+        // Given
+        Car car1 = new Car(new CarName("pobi"));
+        Car car2 = new Car(new CarName("woni"));
+
+        // When
+        // 각 자동차에 대해 랜덤 숫자에 따른 움직임 판단
+        racingGame.jundgeMovement(car1, MOVING_FORWARD);
+        racingGame.jundgeMovement(car2, STOP);
+
+        // Then
+        // 랜덤 숫자에 따라 자동차가 움직였는지 확인
+        assertThat(car1.getPosition()).isEqualTo(1); // MOVING_FORWARD일 때 위치 증가
+        assertThat(car2.getPosition()).isEqualTo(0); // STOP일 때 위치 유지
+    }
+}
