34a13890d2896d3394d210054abb0997afd489c5
나의찬 <cab07036@naver.com>
2025-10-26 02:25:27 +0900

    feat: 전체 기능들 통합
    Application에서 Controller를 통해서 실행을 합니다.
    
    그 이후 Controller에서 자동차 이름 입력 받은 수 검증을 거치고 자동차 객체들을 생성합니다.
    
    그리고 시도 횟수를 입력받고 시도 횟수 객체를 생성합니다.
    
    마지막으로, racingGame.run을 통해서 자동차 객체들과 시도횟수 객체를 전달하여 게임을 진행합니다.

---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,40 @@
+package racingcar.controller;
+
+import racingcar.domain.Attempt;
+import racingcar.domain.CarFactory;
+import racingcar.domain.Cars;
+import racingcar.domain.service.RacingGame;
+import racingcar.util.InputParser;
+import racingcar.util.InputValidator;
+import racingcar.view.InputView;
+
+public class RacingCarController {
+
+    private final InputView inputView;
+    private final CarFactory carFactory;
+    private final RacingGame racingGame;
+
+    public RacingCarController() {
+        this.inputView = new InputView();
+        this.carFactory = new CarFactory();
+        this.racingGame = new RacingGame();
+    }
+
+    public void run() {
+        // 자동차 이름 입력 받기
+        String carNamesInput = inputView.inputCarNames();
+        InputValidator.validateInputFormat(carNamesInput);
+        String[] carNames = InputParser.parseStringToArray(carNamesInput);
+
+        // 자동차 객체 생성
+        Cars cars = carFactory.createCars(carNames);
+
+        // 시도 횟수 입력 받기
+        String attemptCountInput = inputView.inputAttemptCount();
+        Attempt attempt = new Attempt(attemptCountInput);
+
+        // 게임 실행
+        racingGame.run(cars, attempt);
+    }
+}
+
