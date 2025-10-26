93d730fd209bd48c96bb8af87d9f48868763d4a5
나의찬 <cab07036@naver.com>
2025-10-26 22:42:10 +0900

    refactor(util): 입력 문자열 검증 수정
    각 입력이 현재 알파벳과숫자만 들어갈 수 있는 형태였습니다.
    
    하지만 요구사항을 확인해보니 그러한 제약사항이 없었기 때문에 쉼표, 공백을 제외한 (한글, 알파벳, 숫자, 특수문자(쉼표, 공백 제외)) 문자들을 형식으로 수정하였습니다.

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -4,7 +4,10 @@ public class InputValidator {
 
     public static boolean validateInputFormat(String input) {
 
-        String regex = "^[a-zA-Z0-9]+(,[a-zA-Z0-9]+)*$";
+        // 각 이름은 1~4자의 문자
+        // 쉼표와 공백을 제외한 모든 문자
+        // 쉼표로 구분된 문자열
+        String regex = "^[^,\\s]{1,4}(,[^,\\s]{1,4})*$";
 
         if (!input.matches(regex)) {
             throw new IllegalArgumentException("입력 형식이 올바르지 않습니다.");
