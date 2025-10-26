ade412c4896539fcbee2a29e6262c3e5278e71f4
나의찬 <cab07036@naver.com>
2025-10-26 22:21:57 +0900

    refactor(test): 입력값 검증 부분 범위 확장
    현재 입력값이 영어숫자만 허용하고 있었습니다.
    
    하지만 문제에서는 그러한 제약사항이 없기에 영어,숫자,한글,특수문자까지 허용할 수 있도록 테스트 코드를 작성하였습니다.

--- src/test/java/racingcar/util/InputValidatorTest.java
+++ src/test/java/racingcar/util/InputValidatorTest.java
@@ -9,9 +9,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputValidatorTest {
 
-    @Test
-    void 입력형식_검증_테스트() {
-        String input = "pobi,woni,jun";
+    // 한글, 영문, 숫자, 쉽표 제외한 특수문자 입력 테스트
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "pobi,woni,jun!", "pobi,woni,ju@n", "pobi,woni,ju#n", "pobi,woni,ju$n",
+            "pobi,woni,ju%n", "pobi,woni,ju^n", "pobi,woni,ju&n", "pobi,woni,ju*n",
+            "pobi,woni,ju(n", "pobi,woni,ju)n", "pobi,woni,ju-n", "pobi,woni,ju+n",
+            "pobi,woni,ju=n", "pobi,woni,ju{n", "pobi,woni,ju}n", "pobi,woni,ju[n",
+            "pobi,woni,ju]n", "pobi,woni,ju|n", "pobi,woni,ju\\n", "pobi,woni,ju:n",
+            "pobi,woni,ju;n", "pobi,woni,ju\"n", "pobi,woni,ju'n", "pobi,woni,ju<n",
+            "pobi,woni,ju>n", "pobi,woni,ju,n", "pobi,woni,ju.n", "pobi,woni,ju?n",
+            "pobi,woni,ju/n"
+    })
+    void 입력형식_검증_테스트(String input) {
         boolean isValid = InputValidator.validateInputFormat(input);
 
         assertThat(isValid).isTrue();
